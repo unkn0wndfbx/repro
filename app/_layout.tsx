738fd35da4ca4d48952e3aea8c43a37d6997d4f1
unkn0wnd.fbx <titouan.helbert@gmail.com>
2025-09-17 00:28:22 +0200

    update depency to reflect original repo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,32 @@
 import '@/global.css';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import * as Sentry from '@sentry/react-native';
+import 'expo-dev-client';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+
+Sentry.init({
+  dsn: '',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+
+  // Désactiver Sentry en développement
+  enabled: !__DEV__,
+  environment: __DEV__ ? 'development' : 'production',
+});
 
 export const unstable_settings = {
   anchor: '(tabs)',
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,13 @@
-const { getDefaultConfig } = require('expo/metro-config')
 const { withNativeWind } = require('nativewind/metro')
+const { getSentryExpoConfig } = require('@sentry/react-native/metro')
 
-const config = getDefaultConfig(__dirname)
+const config = getSentryExpoConfig(__dirname)
+
+config.resolver = {
+  ...config.resolver,
+  sourceExts: [...config.resolver.sourceExts, 'svg'],
+  assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
+  resetCache: true
+}
 
 module.exports = withNativeWind(config, { input: './global.css' })
