ad6c96505668688ac611f8d98f69703f498a4045
unkn0wnd.fbx <titouan.helbert@gmail.com>
2025-09-16 23:56:22 +0200

    bug fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ export default function HomeScreen() {
   return (
     <SafeAreaView>
       <View className='w-full flex flex-col gap-2  items-start justify-center relative pb-2'>
-        <Text className='text-white text-2xl w-full font-extrabold leading-none'>
+        <Text className='text-red text-2xl w-full font-extrabold leading-none'>
           Azerty
         </Text>
       </View>
